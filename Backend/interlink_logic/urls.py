f8c366d4a1d334617e6e994e295c7260050f675c
OtienoOdongo <otabiznake@gmail.com>
2025-05-25 16:52:09 +0300

    backend work update may 25th 2025

--- Backend/interlink_logic/urls.py
+++ Backend/interlink_logic/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('api/user_management/', include('user_management.api.urls')),
     #path('api/support/', include('support.urls')),
-    path('api/payments/', include('payments.api.urls')),
+    #path('api/payments/', include('payments.api.urls')),
     path('api/network_management/', include('network_management.api.urls')),
     path('api/internet_plans/', include('internet_plans.api.urls')),
     # path('api/dashboard/', include('dashboard.urls')),
